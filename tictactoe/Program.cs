8465ce4ee3907e5679f6e8e7a29ca204dc2ce82d
Sly <85970858+SlyNinjutsu07@users.noreply.github.com>
2024-06-01 17:24:40 -0400

    Update Program.cs
    finished

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -1,21 +1,20 @@
-﻿using System;
+using System;
 
 public class Program{
 
     public static void Main(string[] args){
+
         string startMessage = "Type \"start\" to begin:";
-        string quitMessage = "Type \"quit\" to quit game:";
+        string quitMessage = "Type \"quit\" to quit game";
+
         Console.WriteLine(startMessage);
 
         Input:
         string? input = Console.ReadLine();
-        try{
-            if(input==null){
-                Console.WriteLine(startMessage);
-                goto Input;
-            } 
-        } catch(IOException){
+        
+        if(input==null || input?.ToLower() != "start"){
             Console.Clear();
+            Console.WriteLine(startMessage);
             goto Input;
         }
 
@@ -23,14 +22,26 @@ public class Program{
 
         while(!game.checkForWin()){
             game.turn = !game.turn;
+
+            Console.WriteLine("\n" + game.Player + "'s turn!\n");
+
             game.printBoard();
 
-            Console.WriteLine(quitMessage + "\n");
+            Console.WriteLine($"({quitMessage})\n");
 
             input = Console.ReadLine();
             if(input?.ToLower() == "quit") break;
             readInput(game, input);
-        }    
+        }
+
+        if(game.checkForWin()){
+            if(game.turn == true)
+                Console.WriteLine("Player 1 won!");
+            else    
+                Console.WriteLine("Player 2 won!");
+        }
+
+        Console.ReadKey();
     }
 
     public static void readInput(Game g, string? input){
